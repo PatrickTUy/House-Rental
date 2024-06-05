d9efcec9e62b95cd1fa43f0bd085a452a8054d29
Patrick <yangeney@gmail.com>
2024-06-05 19:31:20 +0200

    added update house service

--- src/modules/house/house.service.ts
+++ src/modules/house/house.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { QueryRepository } from 'src/database/neo4j/query.repository';
-import { CreateHouseInput } from './types/house.create.types';
+import { CreateHouseInput, UpdateHouseInput } from './types/house.create.types';
 import { generateUuid } from 'src/utils/uuid.util';
 import { CloudinaryService } from 'src/config/cloudinary/cloudinary.service';
 
@@ -106,6 +106,102 @@ export class HouseService {
       }
     } catch (error) {
       console.log('Error creating a house:', error);
+      throw new HttpException(
+        error.message || 'Internal Server Error',
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async updateHouse(
+    houseId: string,
+    updateHouseInput: UpdateHouseInput,
+    files: { photos: Express.Multer.File[] },
+  ): Promise<any> {
+    const {
+      name,
+      description,
+      amount,
+      location,
+      type,
+      rooms,
+      baths,
+      propertySize,
+      lotSize,
+    } = updateHouseInput;
+
+    try {
+      if (
+        type !== 'apartment' &&
+        type !== 'villa' &&
+        type !== 'office' &&
+        type !== 'bungalow' &&
+        type !== 'house'
+      ) {
+        throw new HttpException(
+          'House type is not valid',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      const images = [];
+      for (let i = 0; i < files.photos?.length; i++) {
+        const image = files.photos[i];
+        const file = await this.cloudinaryService
+          .uploadImage(image)
+          .catch((err) => {
+            throw new HttpException(err, HttpStatus.BAD_REQUEST);
+          });
+        images.push(file?.url);
+      }
+
+      const house = await this.queryRepo
+        .initQuery()
+        .raw(
+          `
+        MATCH (house:House {id: $houseId})
+        SET house.name = $name,
+            house.description = $description,
+            house.location = $location,
+            house.type = $type,
+            house.amount = $amount,
+            house.rooms = $rooms,
+            house.baths = $baths,
+            house.propertySize = $propertySize,
+            house.lotSize = $lotSize,
+            house.photos = $photos
+        RETURN house
+        `,
+          {
+            houseId,
+            name,
+            description,
+            location,
+            type,
+            amount,
+            rooms,
+            baths,
+            propertySize,
+            lotSize,
+            photos: images,
+          },
+        )
+        .run();
+
+      if (house?.length > 0) {
+        const {
+          house: { properties },
+        } = house[0];
+
+        return {
+          id: properties.id,
+          ...properties,
+        };
+      } else {
+        throw new HttpException('House not found', HttpStatus.NOT_FOUND);
+      }
+    } catch (error) {
+      console.log('Error updating the house:', error);
       throw new HttpException(
         error.message || 'Internal Server Error',
         error.status || HttpStatus.INTERNAL_SERVER_ERROR,
--- src/modules/house/types/house.create.types.ts
+++ src/modules/house/types/house.create.types.ts
@@ -65,3 +65,54 @@ export class CreateHouseInput {
   @IsUUID()
   readonly ownerId: string;
 }
+
+@InputType()
+export class UpdateHouseInput {
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  @IsDefined()
+  readonly name: string;
+
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  @IsDefined()
+  readonly description: string;
+
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  @IsDefined()
+  readonly location: string;
+
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  @IsDefined()
+  readonly amount: string;
+
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  @IsDefined()
+  readonly type: string;
+
+  @Field()
+  @IsNumber()
+  readonly rooms: number;
+
+  @Field()
+  @IsNumber()
+  readonly baths: number;
+
+  @Field()
+  @IsNumber()
+  readonly lotSize: number;
+
+  @Field()
+  @IsNumber()
+  @IsNotEmpty()
+  @IsDefined()
+  readonly propertySize: number;
+}
